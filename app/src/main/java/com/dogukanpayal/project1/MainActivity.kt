0af0dd5b44ac733050ac71891e54a3e758b28b5d
dogukanpayal <payaldogukan@gmail.com>
2025-05-09 18:26:55 +0300

    İlk ders

---
+++ app/src/main/java/com/dogukanpayal/project1/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.dogukanpayal.project1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        println("Hello World")
+        println("Dogukan Payal")
+        // yorum satırı
+
+        var x = 10
+        println(x)
+
+        var y = 20
+        println(x+y)
+
+        // val Kullanılırsa o değeri sonradan değiştiremezsin fix bir değer
+        val z =30
+        println(z)
+
+        var a : Int = 20
+        var f : Float = 20.2f
+
+        val pi = 3.14
+
+        println(f)
+        println(pi)
+
+        val stringKelime = "Dogukan Payal"
+        val stringKelime2 = "Bilgisayar Mühendisi"
+        println(stringKelime)
+
+        println( stringKelime.uppercase())
+        println(stringKelime +" "+ stringKelime2)
+
+        val benimStr : String
+        benimStr = "Benim Stringim" // init
+        println(benimStr)
+
+        // donusturme
+        val yas = "15"
+        println(yas.toInt() * 20)
+
+        val benimBool : Boolean = true
+
+        val benimBool2 = true
+
+        println(benimBool2)
+        println(benimBool)
+
+        val kullaiciYas = "35"
+        println(kullaiciYas.toInt()>40)
+
+        val arrayOrnegi = arrayOf("Dogukan","Payal")
+
+        println(arrayOrnegi[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
